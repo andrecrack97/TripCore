acb6aa15541cda1f46ee6a7026e37dbad0705ef5
FRANCISCO SCHREIBER <48590414@ort>
2025-07-07 12:06:07 -0300

    cmbios

--- TC/src/views/Registro/Registro.jsx
+++ TC/src/views/Registro/Registro.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import "./Registro.css";
-
+import React from 'react';
+import './Registro.css'; 
 export default function Registro() {
-    return (
-      <div className="login-page">
-        <div className="login-box">
-          <div className="login-form">
-            <h1>¡Registrate<br />viajero!</h1>
-            <input type="text" placeholder="Nombre completo" />
-            <input type="email" placeholder="Correo electrónico" />
-            <input type="password" placeholder="Contraseña" required/>
-            <input type="password" placeholder="Confirmar contraseña" required/>
-            <input type="text" placeholder="País de origen"/>
-            <a href="/views/Login/Login" className="link">¿Ya tienes una cuenta? Inicia sesión.</a>
-            <button>Registrarse</button>
-          </div>
-          <div className="login-image">
-            <img src="/assets/iniciosesion.png" alt="Viaje" />
-          </div>
+  return (
+    <div className="registro-page">
+      <div className="registro-box">
+        <div className="registro-form">
+          <h1>¡Registrate viajero!</h1>
+          <input type="text" placeholder="Nombre completo" />
+          <input type="email" placeholder="Correo electrónico" />
+          <input type="password" placeholder="Contraseña" />
+          <input type="password" placeholder="Confirmar contraseña" />
+          <input type="text" placeholder="País de origen" />
+          <a href="/Login" className="link">¿Ya tienes una cuenta? Inicia sesión.</a>
+          <button>Registrarse</button>
+        </div>
+        <div className="registro-image">
+          <img src="/assets/registrarse.png" alt="Viaje" />
         </div>
       </div>
-    );
+    </div>
+  );
 }
